9db4202ea7a4eac22d845500ed6d8fd0270d205e
jotacarvalh0 <josecarvalho11235@gmail.com>
2024-05-06 16:26:21 -0300

    User entity and resource

---
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -0,0 +1,22 @@
+package com.educandoweb.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.educandoweb.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Jota", "josecarvalho11235@gmail.com", "88988704467", "barcelona2018");
+		return ResponseEntity.ok().body(u);
+	}
+}
+
+
+
