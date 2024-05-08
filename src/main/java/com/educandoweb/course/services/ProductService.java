620a6950ad9dcc50c677c92357c73a65ffe30c85
jotacarvalh0 <josecarvalho11235@gmail.com>
2024-05-08 17:43:26 -0300

    Product

---
+++ src/main/java/com/educandoweb/course/services/ProductService.java
@@ -0,0 +1,28 @@
+package com.educandoweb.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.course.entities.Product;
+import com.educandoweb.course.repositories.ProductRepository;
+
+
+@Service
+public class ProductService {
+
+	@Autowired //Para que o spring faça a injeção de dependencia de forma transparente ao programador.
+	private ProductRepository repository;
+	
+	public List<Product> findALl() {
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
