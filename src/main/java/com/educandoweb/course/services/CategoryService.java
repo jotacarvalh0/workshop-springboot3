b00eeff89f53b251e5baeee1dbd150ee3fc49c17
jotacarvalh0 <josecarvalho11235@gmail.com>
2024-05-08 17:08:30 -0300

    Category

---
+++ src/main/java/com/educandoweb/course/services/CategoryService.java
@@ -0,0 +1,28 @@
+package com.educandoweb.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.course.entities.Category;
+import com.educandoweb.course.repositories.CategoryRepository;
+
+
+@Service
+public class CategoryService {
+
+	@Autowired //Para que o spring faça a injeção de dependencia de forma transparente ao programador.
+	private CategoryRepository repository;
+	
+	public List<Category> findALl() {
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
